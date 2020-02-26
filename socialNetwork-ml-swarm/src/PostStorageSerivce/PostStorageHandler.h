c2cdefee862fdfe5b4855323912ab9e07a670476
MyUbuntu <yz2297@cornell.edu>
2020-02-25 23:25:46 -0800

    debug thread

--- socialNetwork-ml-swarm/src/PostStorageSerivce/PostStorageHandler.h
+++ socialNetwork-ml-swarm/src/PostStorageSerivce/PostStorageHandler.h
@@ -50,6 +50,8 @@ void PostStorageHandler::StorePost(
     int64_t req_id, const social_network::Post &post,
     const std::map<std::string, std::string> &carrier) {
 
+  LOG(error) << "StorePost req_id = " << req_id << "post_id = " << post.post_id;
+
   // Initialize a span
   TextMapReader reader(carrier);
   std::map<std::string, std::string> writer_text_map;
@@ -61,8 +63,9 @@ void PostStorageHandler::StorePost(
   opentracing::Tracer::Global()->Inject(span->context(), writer);
 
   // do not record mongodb insert time here since its asynchronous
-  std::thread([&]
+  std::thread([]
     {
+      LOG(error) << "StorePost thread req_id = " << req_id << "post_id = " << post.post_id;
       mongoc_client_t *mongodb_client = mongoc_client_pool_pop(
           _mongodb_client_pool);
       if (!mongodb_client) {
